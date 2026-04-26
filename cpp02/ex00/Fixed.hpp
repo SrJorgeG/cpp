e088125bccd34b1312f0a88579383736e28f5666
Jorge-Windows-Portatil <sr.g.jorge26@gmail.com>
2026-04-26 22:29:35 +0200

    cpp02/ex00 añadido

---
+++ cpp02/ex00/Fixed.hpp
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <iostream>
+
+class Fixed
+{
+private:
+	int _rawBits;
+	static const int _decimals = 8;
+public:
+	Fixed();
+	Fixed(const Fixed& other);
+	Fixed& operator=(const Fixed& other);
+	~Fixed();
+	int		getRawBits( void ) const;
+	void	setRawBits( int const raw );
+
+};
